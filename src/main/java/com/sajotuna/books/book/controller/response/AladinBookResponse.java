f383f7b221a763207a6dba043ce18cd95bb41d29
jju <dlguswn8254@naver.com>
2025-07-18 15:57:50 +0900

    [feat] : 재고 적용

--- src/main/java/com/sajotuna/books/book/controller/response/AladinBookResponse.java
+++ src/main/java/com/sajotuna/books/book/controller/response/AladinBookResponse.java
@@ -24,7 +24,7 @@ public class AladinBookResponse {
     private String cover;
     private String publisher;
     private String categoryName;
-    private Integer stock;
+    private Integer stock = 100;
 
     public List<String> getCategoryNames() {
         if(categoryName == null || categoryName.isBlank()) {
